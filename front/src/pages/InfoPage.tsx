d8cb3f691b58c969ad9d085a2a0a780478290ade
Noa Dudai <nooninana@gmail.com>
2025-01-04 14:05:21 +0200

    info page

--- front/src/pages/InfoPage.tsx
+++ front/src/pages/InfoPage.tsx
@@ -7,31 +7,10 @@ import {Link} from "react-router-dom";
 
 const InfoPage = () => {
 
-    const { loginWithRedirect, isAuthenticated, getAccessTokenSilently } = useAuth0();
-    const [employee, setEmployee] = useState(false);
-
-    const sendEmployeeDataToDB = async () => {
-        try {
-            const serverUrl = envs.envs["REACT_APP_MY_APP_SERVER_URL"];
-
-            const token = await getAccessTokenSilently();
-            const response = await axios.post(`${serverUrl}/savenewemployee`, {}, {
-                headers: { 'Authorization': `Bearer ${token}` },
-            })
-
-            console.log(response.data);
-            setEmployee(true);
-
-        } catch (error) {
-            console.log(error);
-            setEmployee(false);
-        }
-    };
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
 
     useEffect(() => {
-        if (isAuthenticated) {
-            sendEmployeeDataToDB();
-        }
+
     }, [isAuthenticated]);
 
     return (
@@ -43,15 +22,13 @@ const InfoPage = () => {
                 </div>
             ) : (
                 <div>
-                    { employee ? (
-                        <div className="grid justify-items-center p-20">
-                            <p className="text-8xl">Welcome</p>
-                            <div
-                                className="bg-green-300 text-green-950 font-medium border border-green-950 hover:bg-green-200 hover:text-green-900 px-3 py-2 mx-0.5 rounded-lg">
-                                <Link to="/">Home page</Link>
-                            </div>
+                    <div className="grid justify-items-center p-20">
+                        <p className="text-8xl">Welcome</p>
+                        <div
+                            className="bg-green-300 text-green-950 font-medium border border-green-950 hover:bg-green-200 hover:text-green-900 px-3 py-2 mx-0.5 rounded-lg">
+                            <Link to="/">Home page</Link>
                         </div>
-                    ) : (<p>Signing you in..</p>)}
+                    </div>
                 </div>
             )}
         </div>
