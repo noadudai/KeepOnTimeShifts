123cb41e6c4c946df642dc578f2d87827aeb1292
Noa Dudai <nooninana@gmail.com>
2025-03-25 22:46:09 +0200

    redirect to home page

--- front/src/pages/InfoPage.tsx
+++ front/src/pages/InfoPage.tsx
@@ -11,19 +11,9 @@ const InfoPage = () => {
     return (
         <div>
             <InfoPageNavBar isAuthenticated={isAuthenticated} loginWithRedirect={loginWithRedirect} />
-            {!isAuthenticated ? (
+            {!isAuthenticated && (
                 <div className="justify-items-center">
                     <p className="text-center p-20 text-7xl">Hello!</p>
-                </div>
-            ) : (
-                <div>
-                    <div className="grid justify-items-center p-20">
-                        <p className="text-8xl">Welcome</p>
-                        <div
-                            className="bg-green-300 text-green-950 font-medium border border-green-950 hover:bg-green-200 hover:text-green-900 px-3 py-2 mx-0.5 rounded-lg">
-                            <Link to="/">Home page</Link>
-                        </div>
-                    </div>
                 </div>
             )}
         </div>
