9684955cea845832939c1f4d8f7fa550b723233d
Noa Dudai <nooninana@gmail.com>
2025-06-07 01:03:42 +0300

    box with dimmed bg

--- front/src/components/CreateShiftsPanel.tsx
+++ front/src/components/CreateShiftsPanel.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
-const CreateShiftsPanel = () => {
-    return (
-        <div className="inset-0 bg-opacity-30 backdrop-brightness-150">
-            <div></div>
+const CreateShiftsPanel = ({onClose}: { onClose: () => void }) => {
+    const nextWeeksDayDates: string[] = []
+    const daysInWeek: number = 7;
 
+    const today: Date = new Date();
+
+    const nextSunday = daysInWeek - today.getDay();
+
+    for (let i = 0; i < 7; i++) {
+        const dayInNextWeek = new Date(today)
+        dayInNextWeek.setDate(dayInNextWeek.getDate() + nextSunday + i);
+        nextWeeksDayDates.push(dayInNextWeek.toDateString());
+    }
+
+    const dates = nextWeeksDayDates.map((date) => <div className="flex items-center justify-center bg-lime-300 rounded-xl p-2 text-xs">{date}</div>);
+    // <ul>{dates}</ul>
+
+    return (
+        <div className="fixed inset-0 bg-black/10 flex justify-center items-center ">
+            <div className="w-2/3 bg-white rounded-xl p-2">
+                <div className="bg-amber-100 rounded-xl p-2 justify-center flex items-center">
+                    <h1 className="text-2xl">Create Next Week's Shifts <span
+                        className="italic text-xs">{nextWeeksDayDates.at(0)} - {nextWeeksDayDates.at(nextWeeksDayDates.length - 1)} </span>
+                    </h1>
+                </div>
+                <div className="p-2 flex items-center justify-center">
+                    <div className="flex flex-row gap-2 ">{dates}</div>
+                </div>
+            </div>
         </div>
     );
 };
--- front/src/pages/AdminPage.tsx
+++ front/src/pages/AdminPage.tsx
@@ -1,25 +1,10 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import {CreateShiftsNavbar} from "../components/CreateShiftsNavbar.tsx";
 import { IoMdTime } from "react-icons/io";
 import CreateShiftsPanel from "../components/CreateShiftsPanel.tsx";
 
 const AdminPage = () => {
-
-    const nextWeeksDayDates: string[] = []
-    const daysInWeek: number = 7;
-
-    const today: Date = new Date();
-
-    const nextSunday = daysInWeek - today.getDay();
-
-    for (let i = 0; i < 7; i++) {
-        const dayInNextWeek = new Date(today)
-        dayInNextWeek.setDate(dayInNextWeek.getDate() + nextSunday + i);
-        nextWeeksDayDates.push(dayInNextWeek.toDateString());
-    }
-
-    const dates = nextWeeksDayDates.map((date) => <li>{date}</li>);
-    // <ul>{dates}</ul>
+    const [isCreateNewShiftsModalOpen, setIsCreateNewShiftsModalOpen] = useState(false);
 
     return (
         <div>
@@ -29,12 +14,16 @@ const AdminPage = () => {
                 <div className="w-2/3 m-20 rounded-xl bg-white">
                     <button
                         className="border-4 border-yellow-200 rounded-xl p-6 gap-4 flex flex-col items-center justify-center"
-                        onClick={() => <CreateShiftsPanel/>}>
+                        onClick={() => setIsCreateNewShiftsModalOpen(true)}
+                    >
                         <p className="text-black font-semibold">Next Week's Shifts</p>
                         <IoMdTime size={50}/>
                     </button>
                 </div>
             </div>
+            {isCreateNewShiftsModalOpen && (
+                <CreateShiftsPanel onClose={() => {setIsCreateNewShiftsModalOpen(false)}}/>
+            )}
 
         </div>
     );
