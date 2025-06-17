42c62d9bfd9f9da1979c9e6c1e1fc46be0670b02
Noa Dudai <nooninana@gmail.com>
2025-06-17 22:14:21 +0300

    Admin panel navbar + page

---
+++ front/src/pages/AdminPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminPanelNavbar from "../components/AdminPanelNavbar.tsx";
+
+const AdminPage = () => {
+    return (
+        <div>
+            <AdminPanelNavbar />
+        </div>
+    );
+};
+
+export default AdminPage;
