91b8964138638a2b92a287a9d690e8fe049c21f6
Noa Dudai <nooninana@gmail.com>
2025-07-30 16:46:43 +0300

    Keep state of shifts until submit (save to db)

--- front/src/components/WeeklyShiftCreatorPanel.tsx
+++ front/src/components/WeeklyShiftCreatorPanel.tsx
@@ -1,62 +1,32 @@
 import {useState} from 'react';
 import {IoIosCheckmark} from "react-icons/io";
 import {Guid} from "guid-typescript";
-import {useCreateNewShiftsSchedule} from "../apis.ts";
-import {ScheduleModel} from "@noadudai/scheduler-backend-client";
 import {ShiftScheduleGrid} from "./ScheduleAndShiftsCreationComponents/ShiftScheduleGrid.tsx";
-
-
+import { IoClose } from "react-icons/io5";
 import {EditingShift, ShiftMetadata, SHIFTTYPES} from "./ScheduleAndShiftsCreationComponents/Types.ts";
 import {EditingShiftPane} from "./ScheduleAndShiftsCreationComponents/EditingShiftPane.tsx";
 
+type Props = {
+    onClose: () => void;
+    saveEditingShiftToSchedule: (id: Guid, start: Date, end: Date) => void
+    shiftsSchedule: ShiftMetadata[];
+    nextWeeksDayDates: Date[];
+    onSubmitSchedule: () => void;
+};
 
-const WeeklyShiftCreatorPanel = ({onClose}: { onClose: () => void }) => {
-
-    const daysInWeek: number = 7;
-    const today: Date = new Date();
-    const nextSunday = daysInWeek - today.getDay();
+const WeeklyShiftCreatorPanel = ({ onClose, saveEditingShiftToSchedule, shiftsSchedule, nextWeeksDayDates, onSubmitSchedule }: Props) => {
 
     const [editingShift, setEditingShift] = useState<EditingShift | undefined>(undefined);
-
-    const nextWeeksDayDates: Date[] = Array.from({length: daysInWeek}, (_, i) => new Date(today.getFullYear(), today.getMonth(), today.getDate() + nextSunday + i));
-
-    const initialState: ShiftMetadata[] = Array.from(Object.values(SHIFTTYPES), (type) => nextWeeksDayDates.map((date) => {
-        return ({id: Guid.create(), shiftType:type, startTime: date, endTime: undefined});
-    })).flat();
-
-    const [shiftsSchedule, setShiftsSchedule] = useState<ShiftMetadata[]>(initialState);
-
-    const mutation = useCreateNewShiftsSchedule();
 
     const days = nextWeeksDayDates.map(date =>
         <p key={date.toISOString()} className="bg-custom-pastel-green rounded-lg items-center text-center italic pb-1">
             {date.toLocaleDateString('en-us', { weekday: 'short', day: "numeric", month: "numeric" })}
         </p>);
 
-    const saveEditingShiftCallBack = (shiftId: Guid, startTime: Date, endTime: Date) => {
-        setShiftsSchedule(prev =>
-            prev.map((shift) =>
-                shift.id === shiftId ? { ...shift, startTime: startTime, endTime: endTime } : shift));
-
+    const saveEditingShiftToScheduleCallBack = (shiftId: Guid, startTime: Date, endTime: Date) => {
+        saveEditingShiftToSchedule(shiftId, startTime, endTime);
         setEditingShift(undefined);
     }
-
-    const onSubmitSchedule = () => {
-        // Only the shifts that have a defined endTime, are shifts that the manager created for the schedule.
-        const shiftsForMutation = shiftsSchedule.filter((shift)  => shift.endTime !== undefined);
-
-        if (shiftsForMutation.length > 0) {
-            const data: ScheduleModel = {shifts: shiftsForMutation.map((shift) =>
-                    ({
-                        shiftStartTime: shift.startTime.toISOString(),
-                        shiftEndTime: shift.endTime!.toISOString(),
-                        shiftType: shift.shiftType
-                    }))};
-
-            mutation.mutate(data);
-        }
-        onClose();
-    };
 
     const updateEditingShiftStartTime = (date: Date) => {
         setEditingShift(prev => prev ? { ...prev, startTime: date } : prev);
@@ -69,6 +39,7 @@ const WeeklyShiftCreatorPanel = ({onClose}: { onClose: () => void }) => {
     return (
         <div className="flex justify-evenly inset-0 bg-opacity-30 backdrop-blur-sm fixed items-center">
             <div className="flex flex-col w-2/3 bg-white place-self-center items-center p-4 gap-4 border border-gray-200 rounded-lg">
+                <button className="place-self-end bg-custom-pastel-green text-center text-custom-cream rounded-full p-1" onClick={onClose}><IoClose /> </button>
                 <h1 className="text-center text-2xl font-opensans bg-custom-cream-warm w-full p-2 h-full rounded-lg">
                     Create Shifts for next week <br/>
                     <span className="italic text-xs">
@@ -86,7 +57,7 @@ const WeeklyShiftCreatorPanel = ({onClose}: { onClose: () => void }) => {
                 {editingShift &&
                     <EditingShiftPane editingShift={editingShift}
                                                    shiftsSchedule={shiftsSchedule}
-                                                   saveEditingShiftCallBack={saveEditingShiftCallBack}
+                                                   saveEditingShiftCallBack={saveEditingShiftToScheduleCallBack}
                                                    updateEditingShiftStartTime={updateEditingShiftStartTime}
                                                    updateEditingShiftEndTime={updateEditingShiftEndTime}
                     />}
--- front/src/pages/AdminPages/Scheduling.tsx
+++ front/src/pages/AdminPages/Scheduling.tsx
@@ -1,12 +1,51 @@
 import WeeklyShiftCreatorPanel from "../../components/WeeklyShiftCreatorPanel.tsx";
 import {useState} from "react";
+import {Guid} from "guid-typescript";
+import {ShiftMetadata, SHIFTTYPES} from "../../components/ScheduleAndShiftsCreationComponents/Types.ts";
+import {ScheduleModel} from "@noadudai/scheduler-backend-client";
+import {useCreateNewShiftsSchedule} from "../../apis.ts";
 
 const Scheduling = () => {
+    const daysInWeek: number = 7;
+    const today: Date = new Date();
+    const nextSunday = daysInWeek - today.getDay();
+    const nextWeeksDayDates: Date[] = Array.from({length: daysInWeek}, (_, i) => new Date(today.getFullYear(), today.getMonth(), today.getDate() + nextSunday + i));
+
+    const initialState: ShiftMetadata[] = Array.from(Object.values(SHIFTTYPES), (type) => nextWeeksDayDates.map((date) => {
+        return ({id: Guid.create(), shiftType:type, startTime: date, endTime: undefined});
+    })).flat();
+
+    const [shiftsSchedule, setShiftsSchedule] = useState<ShiftMetadata[]>(initialState);
     const [isCreatingShiftsOpen, setIsCreatingShiftsOpen] = useState(false);
 
     const handleCreatingShiftsOpen = () => {
         setIsCreatingShiftsOpen(!isCreatingShiftsOpen);
     }
+
+    const saveEditingShiftToSchedule = (shiftId: Guid, startTime: Date, endTime: Date) => {
+        setShiftsSchedule(prev =>
+            prev.map((shift) =>
+                shift.id === shiftId ? { ...shift, startTime: startTime, endTime: endTime } : shift));
+    };
+
+    const mutation = useCreateNewShiftsSchedule();
+
+    const submitShiftsSchedule = () => {
+        // Only the shifts that have a defined endTime, are shifts that the manager created for the schedule.
+        const shiftsForMutation = shiftsSchedule.filter((shift)  => shift.endTime !== undefined);
+
+        if (shiftsForMutation.length > 0) {
+            const data: ScheduleModel = {shifts: shiftsForMutation.map((shift) =>
+                    ({
+                        shiftStartTime: shift.startTime.toISOString(),
+                        shiftEndTime: shift.endTime!.toISOString(),
+                        shiftType: shift.shiftType
+                    }))};
+
+            mutation.mutate(data);
+        }
+        setIsCreatingShiftsOpen(!isCreatingShiftsOpen);
+    };
 
     return (
         <div className="flex items-center justify-center gap-4 p-2">
@@ -15,9 +54,12 @@ const Scheduling = () => {
                     onClick={handleCreatingShiftsOpen}>Create Next Week's Shifts</button>
                 <button></button>
             </div>
-            {isCreatingShiftsOpen && <WeeklyShiftCreatorPanel onClose={() => {
-                setIsCreatingShiftsOpen(false)
-            }}/>}
+            {isCreatingShiftsOpen && <WeeklyShiftCreatorPanel
+                onClose={() => {setIsCreatingShiftsOpen(false)}}
+                saveEditingShiftToSchedule={saveEditingShiftToSchedule}
+                shiftsSchedule={shiftsSchedule}
+                nextWeeksDayDates={nextWeeksDayDates}
+                onSubmitSchedule={submitShiftsSchedule}/>}
         </div>
     );
 };
